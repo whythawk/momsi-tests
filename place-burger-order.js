675f86cf28f5b7e16f64860ef8d2b319550c055d
Gavin Chait <whythawk@gmail.com>
2025-01-22 11:08:56 +0100

    database

--- place-burger-order.js
+++ place-burger-order.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const order = require('./order.json');
+const data = require('./burger-data.json')
 
 const user = eventPayload.sender.login;
 const [flavour, size, toppings, count] = Object.values(order);
@@ -8,5 +9,8 @@ const [flavour, size, toppings, count] = Object.values(order);
 const amount = parseInt(count.slice(0,3), 10) || 1;
 
 const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} burger with ${toppings}\n`;
+
+data.push(order)
+fs.writeFileSync('burger-data.json', JSON.stringify(data, null, '  '))
 
 fs.appendFileSync('README.md', content);
--- place-pizza-order.js
+++ place-pizza-order.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const order = require('./order.json');
+const data = require('./pizza-data.json')
 
 const user = eventPayload.sender.login;
 const [flavour, size, toppings, count] = Object.values(order);
@@ -8,5 +9,8 @@ const [flavour, size, toppings, count] = Object.values(order);
 const amount = parseInt(count.slice(0,3), 10) || 1;
 
 const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} pizza with ${toppings}\n`;
+
+data.push(order)
+fs.writeFileSync('pizza-data.json', JSON.stringify(data, null, '  '))
 
 fs.appendFileSync('README.md', content);
