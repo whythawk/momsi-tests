7115bd6af6740cf29df55782eb1658e79b8cb25d
Gavin Chait <whythawk@gmail.com>
2025-01-22 11:55:02 +0100

    refactoring

--- place-pizza-order.js
+++ place-pizza-order.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const order = require('./order.json');
-const data = require('./pizza-data.json')
+const data = require('./data.json')
 
 const user = eventPayload.sender.login;
 const [flavour, size, toppings, count] = Object.values(order);
@@ -10,7 +10,7 @@ const amount = parseInt(count.slice(0,3), 10) || 1;
 
 const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} pizza with ${toppings}\n`;
 
-data.push(order)
-fs.writeFileSync('pizza-data.json', JSON.stringify(data, null, '  '))
+data.pizzas.push(order)
+fs.writeFileSync('data.json', JSON.stringify(data, null, '  '))
 
 fs.appendFileSync('README.md', content);
