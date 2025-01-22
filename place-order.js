590606374aadec70870d853fd4182cc6062307fb
Gavin Chait <whythawk@gmail.com>
2025-01-22 16:15:31 +0100

    refactor

--- place-order.js
+++ place-order.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const order = require('./order.json');
-const submitter = require('./submitter.json');
+const submitter = require('./submitter.txt');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
 
 const user = eventPayload.sender.login;
