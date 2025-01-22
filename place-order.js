55fe7d8fa4a99acf0ce4024d0db95921d09f44b3
Gavin Chait <whythawk@gmail.com>
2025-01-22 15:47:11 +0100

    refactor

---
+++ place-order.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+const order = require('./order.json');
+const submitter = require('./submitter.json');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+
+const user = eventPayload.sender.login;
+const [flavour, size, toppings, count] = Object.values(order);
+
+const amount = parseInt(count.slice(0,3), 10) || 1;
+
+const content = `1. [@${user}](https://github.com/${user}) orders ${amount} ${size} ${flavour} burger with ${toppings}\n`;
+
+data[submitter].push(order)
+fs.writeFileSync('data.json', JSON.stringify(data, null, '  '))
+
+fs.appendFileSync('README.md', content);
